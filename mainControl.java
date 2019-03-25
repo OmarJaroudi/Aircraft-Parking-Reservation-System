c16b13ede683a6f3397f972d8c646f6f8520b66f
OmarJaroudi <30807936+OmarJaroudi@users.noreply.github.com>
2019-03-25 12:08:22 +0200

    Updated minor functionalities in each scene
    Created a fourth property for each user which is a unique 4 digit ID number, also stored in the SQL database.
    Created a rudimentary function to check if the email address is valid. The function simply checks if the email address is of reasonable length, and if it contains an '@' and a '.' character

--- mainControl.java
+++ mainControl.java
@@ -17,6 +17,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Random;
 
+
 public class MainControl {
 
 	@FXML
@@ -60,6 +61,7 @@ public class MainControl {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
 			
 		}
@@ -88,12 +90,27 @@ public class MainControl {
 					Scene scene = new Scene(root,400,400);
 					scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 					controlStage.setScene(scene);
+					controlStage.setResizable(false);
 					controlStage.show();
 				}
 			}
 		}
 	}
-	public String randomGen() {
+	public static boolean ValidEmail (String email) {
+		if(email.length()<=8)
+			return false;
+		   for(int i=0;i<email.length();i++) {
+			   if(email.charAt(i)=='@' && i!=0 && i!=email.length()-3) {
+				   for(int j=i+1;j<email.length();j++) {
+					   if(email.charAt(j)=='.' && j!=email.length()-1)
+						   return true;
+				   }
+			   }
+				   
+		   }
+		   return false;
+	}
+	public String randomGenString() {
 		 String word="";
 		    Random random = new Random();
 		      
@@ -113,13 +130,27 @@ public class MainControl {
 			String u = user.getText();
 			String p = pass.getText();
 			String e = email.getText();
+			boolean unique = false;
+			int ID = 0;
+			while(unique==false) {
+				Random rand = new Random();
+				ID = 1000 + rand.nextInt(5000);
+				ResultSet t = statement.executeQuery("SELECT * FROM clientinfo as t WHERE t.ID = '"+ID+"'");
+				if(!t.next())
+					unique = true;
+			}
+			
+			
 			boolean info = false;
 			ResultSet r = statement.executeQuery("SELECT * FROM clientinfo as t WHERE t.username = '"+u+"'");
 			
 			if(!r.next()) {
 				r = statement.executeQuery("SELECT * FROM clientinfo as t WHERE t.email = '"+e+"'");
 				if(!r.next()) {
-					if(pass2.getText().equals(pass.getText())) {
+					if(!ValidEmail(e)) 
+						signupLabel.setText("Email not valid!");
+						
+					else if(pass2.getText().equals(pass.getText())) {
 						signupLabel.setText(" ");
 						info = true;
 					}
@@ -135,17 +166,17 @@ public class MainControl {
 				signupLabel.setText("username already exists!");
 			}
 			
-			boolean bot = true;
+			boolean bot = true; 
 			if(botText.getText().equals(botLabel.getText())) {
 				bot = false;
 				botCheck.setText(" ");
 			}
 			if(info==true && bot==false) {
-				statement.executeUpdate("INSERT INTO clientinfo (username,email,password) VALUE ('"+u+"','"+e+"','"+p+"')");
+				statement.executeUpdate("INSERT INTO clientinfo (username,email,password,ID) VALUE ('"+u+"','"+e+"','"+p+"','"+ID+"')");
 				((Node)(event.getSource())).getScene().getWindow().hide();
 			}
 			else {
-				String w = randomGen();
+				String w = randomGenString();
 				botLabel.setText(w);
 				botCheck.setText("Invalid expression");
 			}
