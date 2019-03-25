8456eb4a38538d4d5cc691ab5e8829af3e9197d1
OmarJaroudi <30807936+OmarJaroudi@users.noreply.github.com>
2019-03-25 12:03:42 +0200

    Create ConnectionClass.java
    Connection class that connects user to mySQL server through jdbc

---
+++ ConnectionClass.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionClass {
+public Connection connection;
+    public  Connection getConnection(){
+
+
+        String dbName="clients";
+        String userName="root";
+        String password="";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+
+        connection= DriverManager.getConnection("jdbc:mysql://localhost/"+dbName,userName,password);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return connection;
+    }
+
+}
